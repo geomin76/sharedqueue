6c6324b16d3e71739bbace55944bb5931f573d36
geomin76 <geom@vt.edu>
2020-04-09 00:56:33 -0400

    data to list in javascript+html

--- test_frontend/script.js
+++ test_frontend/script.js
@@ -8,14 +8,17 @@ fetch("http://localhost:3000/token")
     token = data.token;
 })
 
-function Song(name, songId, img) {
+function Song(name, artist, songId, img) {
     this.name = name;
+    this.artist = artist;
     this.songId = songId;
     this.img = img;
 }
 
 
+
 function myFunction() {
+    var songList = [];
     var x = document.getElementById("mySearch").value;
     var url = 'https://api.spotify.com/v1/search?' + "q=" + encodeURI(x) + "&type=track&market=US&limit=10";
     fetch(url, {
@@ -31,6 +34,32 @@ function myFunction() {
         return response.json()
     })
     .then((data) => {
-        console.log(data.tracks.items);
+        // console.log(data.tracks.items);
+        for (var i = 0; i < 10; i++) {
+            var newSong = new Song(data.tracks.items[i].name, data.tracks.items[i].artists[0].name, data.tracks.items[i].uri, data.tracks.items[i].album.images[0].url);
+            songList[i] = newSong;
+        }
+        var select = document.getElementById("select");
+        var select_length = document.getElementById("select").length;
+        if (select_length == 1) {
+            for (var i = 0; i < 10; i++) {
+                var el = document.createElement("option");
+                el.text = songList[i].name;
+                el.value = songList[i].name;
+                select.add(el);
+            }
+        }
+        else {
+            while (select_length != 0) {
+                select.remove(select_length);
+                select_length--;
+            }
+            for (var i = 0; i < 10; i++) {
+                var el = document.createElement("option");
+                el.text = songList[i].name;
+                el.value = songList[i].name;
+                select.add(el);
+            }
+        }
     })
 }
