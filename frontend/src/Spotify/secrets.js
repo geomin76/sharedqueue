2bc4f32a56aee9ca9440285ba9577771f707245f
Geo Min <31418725+geomin76@users.noreply.github.com>
2020-04-29 21:58:13 -0400

    Delete secrets.js

--- frontend/src/Spotify/secrets.js
+++
@@ -1,4 +0,0 @@
-export const client_id = "8e1e4b013a384ba2be4ff14f80913e97"
-export const redirect_url = "http://localhost:3000"
-export const client_secret = ""
-export const scopes = "streaming user-read-email user-read-private"
